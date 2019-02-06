7480ed5d059c1f08b3b64ccfa2b8ee19449b64d6
Lodi2244 <elodieletellier1@gmail.com>
2019-02-06 19:11:25 +0100

    added and tested validations in model item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,11 +8,34 @@ RSpec.describe Item, type: :model do
 
     describe 'Database' do
       it { is_expected.to have_db_column(:id).of_type(:integer) }
-      it { is_expected.to have_db_column(:name).of_type(:string).with_options(uniqness: true) }
+      it { is_expected.to have_db_column(:title).of_type(:string) }
       it { is_expected.to have_db_column(:description).of_type(:text) }
-      it { is_expected.to have_db_column(:price).of_type(:decimal)
+      it { is_expected.to have_db_column(:price).of_type(:float).with_options(null: false) }
       it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
       it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+    end
+  end
+
+  describe 'Validations' do
+    subject(:item) { create(:item) }
+
+    context 'when factory is valid' do
+      it { expect{ item }.to change(described_class, :count).by(1) }
+      it { is_expected.to be_valid }
+    end
+
+    context 'when an item is created' do
+      subject(:item) { create(:item) }
+
+      it { expect(item).to validate_presence_of(:title) }
+      it { expect(item).to validate_presence_of(:price) }
+      it { expect(item).to validate_presence_of(:description) }  
+      it { expect(item).to validate_uniqueness_of(:title) }
+      it { expect(item).to validate_numericality_of(:price) }
+      it {
+        expect(item).to validate_length_of(:description).
+          is_at_most(100)
+      }
     end
   end
 end
