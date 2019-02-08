bc27cc3b460380ddc126672536e8810d041f94de
Lodi2244 <elodieletellier1@gmail.com>
2019-02-08 17:41:40 +0100

    removed require pry

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,6 @@
 #  updated_at  :datetime         not null
 #
 
-require 'pry'
-
 class Item < ApplicationRecord
   validates :description, presence: true, length: { maximum: 100 }
   validates :title, presence: true, uniqueness: true
