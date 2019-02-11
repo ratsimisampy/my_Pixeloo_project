f34c88700199fd9cb5f61844f36bbd2b7244a4f8
ratsimisampy <ratsimisampy@gmail.com>
2019-02-11 08:58:37 +0100

    devise settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    
+    before_action :configure_devise_parameter, if: :devise_controller?
+
+    def configure_devise_parameter
+
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+
+    end
+
 end
