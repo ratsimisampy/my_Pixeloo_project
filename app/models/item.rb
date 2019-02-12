e5812aadb50c36a57a5a8cf2b7495729722f94ff
ratsimisampy <ratsimisampy@gmail.com>
2019-02-12 20:44:36 +0100

    Active Storage basic settings

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,4 +17,6 @@ class Item < ApplicationRecord
   validates :description, presence: true, length: { maximum: 100 }
   validates :title, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { only_float: true }
+
+  has_many_attached :item_images
 end
