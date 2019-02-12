ecc60654f5ffcb7970399fb621a477c7708c0cdc
Lodi2244 <elodieletellier1@gmail.com>
2019-02-12 13:24:38 +0100

    modified config for Devise Test Helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
