e915d59b5278e59863534adb50d8e8607b701330
ratsimisampy <ratsimisampy@gmail.com>
2019-02-12 10:52:54 +0100

    items migrations and images upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_07_205412) do
+ActiveRecord::Schema.define(version: 2019_02_12_083434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2019_02_07_205412) do
     t.float "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.integer "category_id"
+    t.string "address"
+    t.string "city"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_02_07_205412) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
