f5bd1d0270d19f781f7e8e4e5ed18cfddf77887f
Lodi2244 <elodieletellier1@gmail.com>
2019-02-15 12:42:41 +0100

    customized sign-up view

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,4 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   root 'home#index'
-<<<<<<< HEAD
-=======
-  get 'home/private'
->>>>>>> e635ad6a9678c62a9eadb8f7ec25b371a84406e3
 end
