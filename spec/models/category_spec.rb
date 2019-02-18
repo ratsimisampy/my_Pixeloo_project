51d0c598051871e12e219f43ab37c48cddb344d6
Lodi2244 <elodieletellier1@gmail.com>
2019-02-18 17:53:53 +0100

    second commit after annotated

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  description :string
+#
+
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
