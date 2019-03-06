63668e5b1f3cce82d1cd7f1c9d83d765894b2473
ratsimisampy <ratsimisampy@gmail.com>
2019-03-06 09:18:02 +0100

    add activ column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_021406) do
+ActiveRecord::Schema.define(version: 2019_03_06_081537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_021406) do
     t.string "city"
     t.string "image_url"
     t.bigint "category_id"
+    t.boolean "active"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
